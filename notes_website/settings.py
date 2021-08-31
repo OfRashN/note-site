ff3a796b1fd345c270dce9f1bea79696f3e2b676
Akhmetyanov Arthur <Akhmetyanov.artu.r@yandex.ru>
2021-08-31 13:19:50 +0500

    add secret key

--- notes_website/settings.py
+++ notes_website/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-j11+#m7e^2pe59q009le75!%ftdixee8=oa+7=z8#vn@s+l)7z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
